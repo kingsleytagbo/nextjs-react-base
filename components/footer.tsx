15d945ffa2f026ddfe1846aaeb4aea6769b2fbd4
Kingsley Tagbo <kingsleytagbo@gmail.com>
2022-11-24 21:53:50 -0600

    header & footeer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => (
     <footer className="border-top text-dark text-center pb-2 pt-2 mt-5 bg-light fixed-bottom">
         {
                 <div className="container">
-                    <span className="text-muted">Copyright&nbsp;&copy;&nbsp;{process.env.NEXT_PUBLIC_REACT_APP_WEBSITE_NAM}</span>
+                    <span className="text-muted">Copyright&nbsp;&copy;&nbsp;{process.env.NEXT_PUBLIC_REACT_APP_WEBSITE_NAME}</span>
                 </div>
         }
     </footer>
